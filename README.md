7658d1d7873949b67fc60608af2e267973fb0555
Andrew Stewart <andrew@stwrt.ca>
2019-11-25 17:32:31 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code 2019
+
+Advent of Code 2019 working project. I'm intenting to just use Elixir this year.
---
+++ test/aoc_test.exs
@@ -0,0 +1,8 @@
+defmodule AocTest do
+  use ExUnit.Case
+  doctest Aoc
+
+  test "truthy" do
+    assert true
+  end
+end
