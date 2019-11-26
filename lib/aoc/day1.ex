c0e7cf37c42b641ef8e742661960c22014e85637
Andrew Stewart <andrew@stwrt.ca>
2019-11-25 17:50:34 -0800

    Placeholder for day 01

---
+++ lib/aoc/day1.ex
@@ -0,0 +1,16 @@
+defmodule AOC.Day1 do
+  require Logger
+
+  def go do
+    part1 "hello"
+    part2 "world"
+  end
+
+  defp part1(seed) do
+    Logger.debug(seed)
+  end
+
+  defp part2(seed) do
+    Logger.debug(seed)
+  end
+end
