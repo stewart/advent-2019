988379e7797a66c93e6f12039339eae95a4732b4
Andrew Stewart <andrew@stwrt.ca>
2019-12-01 21:49:32 -0800

    Day 2, Part 1

--- lib/advent/day_2.ex
+++ lib/advent/day_2.ex
@@ -4,7 +4,8 @@ defmodule Advent.Day2 do
   @input "day2"
          |> load_input()
          |> String.trim()
-         |> String.split("\n")
+         |> String.split(",")
+         |> Enum.map(&String.to_integer/1)
 
   def input, do: @input
 
@@ -15,9 +16,37 @@ defmodule Advent.Day2 do
 
   def part1 do
     input()
+    |> List.replace_at(1, 12)
+    |> List.replace_at(2, 2)
+    |> run_program()
+    |> hd()
   end
 
   def part2 do
     input()
+  end
+
+  ## Utils
+
+  def run_program(program, instruction_ptr \\ 0) do
+    case Enum.drop(program, instruction_ptr) do
+      [1, x, y, idx | _] ->
+        x = Enum.at(program, x)
+        y = Enum.at(program, y)
+        updated_program = List.replace_at(program, idx, x + y)
+        run_program(updated_program, instruction_ptr + 4)
+
+      [2, x, y, idx | _] ->
+        x = Enum.at(program, x)
+        y = Enum.at(program, y)
+        updated_program = List.replace_at(program, idx, x * y)
+        run_program(updated_program, instruction_ptr + 4)
+
+      [99 | _] ->
+        program
+
+      other ->
+        raise ArgumentError, "unexpected opcode: #{inspect(other)}"
+    end
   end
 end
---
+++ test/advent/day_2_test.exs
@@ -0,0 +1,16 @@
+defmodule Advent.Day2Test do
+  use ExUnit.Case, async: true
+
+  alias Advent.Day2
+
+  test "part1" do
+    assert Day2.part1 == 4570637
+  end
+
+  test "run_program/1" do
+    assert Day2.run_program([1, 0, 0, 0, 99]) == [2, 0, 0, 0, 99]
+    assert Day2.run_program([2, 3, 0, 3, 99]) == [2, 3, 0, 6, 99]
+    assert Day2.run_program([2, 4, 4, 5, 99, 0]) == [2, 4, 4, 5, 99, 9801]
+    assert Day2.run_program([1, 1, 1, 4, 99, 5, 6, 0, 99]) == [30, 1, 1, 4, 2, 5, 6, 0, 99]
+  end
+end
