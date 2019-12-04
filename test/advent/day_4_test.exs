8197eb6fbe42cdc76624f630190afc51a327c74d
Andrew Stewart <andrew@stwrt.ca>
2019-12-04 08:32:15 -0800

    Day4 solution tests

--- test/advent/day_4_test.exs
+++ test/advent/day_4_test.exs
@@ -3,6 +3,14 @@ defmodule Advent.Day4Test do
 
   alias Advent.Day4
 
+  test "part1" do
+    assert Day4.part1 == 960
+  end
+
+  test "part2" do
+    assert Day4.part2 == 626
+  end
+
   test "valid_password?" do
     assert Day4.valid_password?(111111)
     refute Day4.valid_password?(223450)
